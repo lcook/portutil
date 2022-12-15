e92ada79c057e8c2da9490bf64d13503db6aea7e
Lewis Cook <lcook@FreeBSD.org>
2022-12-15 05:36:55 +0000

    Mk/commit.mk: "Fix" weird behaviour in `git add`ing {un}tracked files
    When passing `-C` to git while running `git add .` with an accompanying
    package directory e.g., `/path/to/ports/devel/gh`, all {un}tracked files
    from the root of the directory are added, rather than the singular
    directory (including sub-directories, files, etc) as defined in
    `${PACKAGE_DIR}`.  I do not know why this is, for now just `cd` into
    `${PACKAGE_DIR}`, instead of passing the absolute path via `git -C ${PACKAGE_DIR}`.

--- Mk/commit.mk
+++ Mk/commit.mk
@@ -7,7 +7,7 @@
 .include "guard.mk"
 
 _GIT=		/usr/local/bin/git
-_GIT_CMD=	${_GIT} -C ${PACKAGE_DIR}
+_GIT_CMD=	${_GIT}
 default:
-	${_GIT_CMD} add .
-	${_GIT_CMD} commit -m "${PACKAGE_ORIGIN}: Update to ${PACKAGE_LATEST}" 
+	@(cd ${PACKAGE_DIR}; ${_GIT_CMD} add .; \
+		${_GIT_CMD} commit -m "${PACKAGE_ORIGIN}: Update to ${PACKAGE_LATEST}")
